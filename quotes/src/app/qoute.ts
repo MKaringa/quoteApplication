e60696df2b8eec82c50495ae4e4f657ccb23ad1a
MKaringa <mungaikaringa1@gmail.com>
2021-05-18 03:57:59 +0300

    Delete due to typo

--- quotes/src/app/qoute.ts
+++
@@ -1,5 +0,0 @@
-export class Qoute {
-    showDescription: boolean;
-    constructor(public id: number, public name:string, public author: string, public blogger:string, public postingDate: Date, public clickCounter: number ){
-        this.showDescription =false;
-}
