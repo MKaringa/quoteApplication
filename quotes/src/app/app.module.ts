dbab693308f1c28c21d199ffcb8809b7a4e732f0
MKaringa <mungaikaringa1@gmail.com>
2021-05-18 10:58:27 +0300

    Import Messages component in the modules

--- quotes/src/app/app.module.ts
+++ quotes/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteFormComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
